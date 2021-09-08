5abb3bce49d1d8a8f7fdfdbff0820b5c960317e3
kousuke <on810521.funabashi@gmail.com>
2021-09-08 21:24:33 +0900

    first commit

---
+++ print.rb
@@ -0,0 +1,25 @@
+print("Hello, Ruby.\n")
+
+print("Hello, Ruby.!!!!\n")
+
+print("hello,
+Ruby
+!
+")
+
+print("hello,\nRuby\n!\n")
+
+print("Hello, \"Ruby\".\n")
+
+print("Hello \\ Ruby.\n")
+
+print('hello, \\ \'Ruby!\'
+')
+
+print"hello\\"
+
+print"Ruby"
+
+print"!!!"
+
+print"\n"
